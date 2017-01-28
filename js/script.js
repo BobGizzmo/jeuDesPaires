b5e5ce286ec9da7fcef97d78a54101812b87e45c
MARTINEZ Kévin <msi03@laposte.net>
2017-01-29 00:34:42 +0100

    MAJ css

--- js/script.js
+++ js/script.js
@@ -3,63 +3,10 @@ var precedImg = "";										//Variables de "sauvegardes"
 var click = 0;											//Varible gerant les click
 var great = 0; var lose = 0;							// Variable victoire et défaite
 var chron;												//Contiendra le chronometre
-var chrono = document.getElementById("chrono");
 var temps = 1; var secondes = 60;
+var chrono = document.getElementById("chrono");
 var partieEnCours = true;								//Gere le lancement du chronometre
 var ready = true;										//Gere l'accès au clicks
-
-function chronometre(){								//FONCTION CHRONOMETRE
-	if(temps >= 0){									
-		secondes--;																//On retire 1 à la variable secondes
-		if(secondes >= 10)
-			document.getElementById("chrono").innerHTML = temps+":"+secondes;	
-		else if(secondes < 10){
-			document.getElementById("chrono").innerHTML = temps+":0"+secondes;
-			if(secondes == 0){
-				secondes = 60;
-				temps--;														//On retire 1 à la variable temps
-			}
-		}
-	}
-	else{
-			lose = 1;															//Si temps == 0 c'est perdu !
-			victoire();
-			clearInterval(chron);
-		}
-}
-function recommencer(victoire){										//FONCTION RECOMMENCER
-	var retry = confirm("Vous avez "+victoire+" !\n Recommencer ?");
-	if(retry){
-		var ZJeu = document.getElementsByClassName("carte");				//Recupère toute les cartes de la Zone de jeu
-		for(i = 0; i < ZJeu.length; i++){
-			ZJeu[i].src = "img/dosCarte.jpg";								//modification de la source de chaque cartes
-			ZJeu[i].setAttribute("onclick", "retourne("+(i+1)+", '"+(i+1)+"')"); //réattribution de la fonction onclick
-		}
-		great = 0; lose = 0;												//réinitialisation de toutes le variables
-		ready = true;
-		partieEnCours = true;
-		temps = 1; secondes = 60;
-		document.getElementById("chrono").innerHTML = "2:00";
-		shuffle(tab);
-	}
-	else{
-		ready = false;
-		great = 0; lose = 0;
-	}
-}
-
-function victoire(){
-	if(great == 6){
-		clearInterval(chron);									//Stop le chronometre <setInterval>
-		recommencer("gagné");									//Appel de la fonction recommencer
-		
-	}
-	else if(lose == 1){
-		ready = false;
-		clearInterval(chron);
-		recommencer("perdu");
-	}
-}
 
 function shuffle(a) {										//Fonction shuffle: melange le tableau tab
     var j, x, i;
@@ -83,7 +30,7 @@ function retourne(id, name){						//FONCTION RETOURNE
 			switch(id){									
 				case i:
 					name.src =tab[i-1];					//Attribution des sources du tableau à chaque images cliquées
-					if(click == 0){						//Au premier et seulement à celui-ci
+					if(click == 0){						//Au premier click et seulement à celui-ci
 						precedImg = name;				//Attribution d'une valeur "sauvegarde" pour comparaison
 						click = 1;						//Empeche de réécrire la sauvegarde au second click
 					}
@@ -112,4 +59,61 @@ function retourne(id, name){						//FONCTION RETOURNE
 	}
 	victoire();
 }
+
+function chronometre(){								//FONCTION CHRONOMETRE
+	if(temps >= 0){									
+		secondes--;																//On retire 1 à la variable secondes
+		if(secondes >= 10)
+			document.getElementById("chrono").innerHTML = temps+":"+secondes;	
+		else if(secondes < 10){
+			document.getElementById("chrono").innerHTML = temps+":0"+secondes;
+			if(secondes == 0){
+				secondes = 60;
+				temps--;														//On retire 1 à la variable temps
+			}
+		}
+	}
+	else{
+			lose = 1;															//Si temps == 0 c'est perdu !
+			victoire();
+			clearInterval(chron);
+		}
+}
+
+function victoire(){
+	if(great == 6){
+		clearInterval(chron);									//Stop le chronometre <setInterval>
+		recommencer("gagné");									//Appel de la fonction recommencer
+		
+	}
+	else if(lose == 1){
+		ready = false;
+		clearInterval(chron);
+		recommencer("perdu");
+	}
+}
+
+function recommencer(victoire){										//FONCTION RECOMMENCER
+	var retry = confirm("Vous avez "+victoire+" !\n Recommencer ?");
+	if(retry){
+		var ZJeu = document.getElementsByClassName("carte");				//Recupère toute les cartes de la Zone de jeu
+		for(i = 0; i < ZJeu.length; i++){
+			ZJeu[i].src = "img/dosCarte.jpg";								//modification de la source de chaque cartes
+			ZJeu[i].setAttribute("onclick", "retourne("+(i+1)+", '"+(i+1)+"')"); //réattribution de la fonction onclick
+		}
+		great = 0; lose = 0;												//réinitialisation de toutes le variables
+		ready = true;
+		partieEnCours = true;
+		temps = 1; secondes = 60;
+		document.getElementById("chrono").innerHTML = "2:00";
+		shuffle(tab);
+	}
+	else{
+		ready = false;
+		great = 0; lose = 0;
+	}
+}
+
+
+
 
