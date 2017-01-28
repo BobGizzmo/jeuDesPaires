fb52910ebcab02d40f4992ef03e6a1ad472b0de3
MARTINEZ Kévin <msi03@laposte.net>
2017-01-28 10:58:28 +0100

    Code_js_refacto + Ajout_de_commentaire

--- js/script.js
+++ js/script.js
@@ -1,41 +1,41 @@
 var tab = ["img/chat.jpg", "img/ane.jpg", "img/lionne.jpg", "img/lapins.jpg", "img/chien.jpg", "img/ours.jpg", "img/chat.jpg", "img/ane.jpg", "img/lionne.jpg", "img/lapins.jpg", "img/chien.jpg", "img/ours.jpg"];
-var precedImg = "";
-var click = 0;
-var great = 0; var lose = 0;
-var chron;
+var precedImg = "";										//Variables de "sauvegardes"
+var click = 0;											//Varible gerant les click
+var great = 0; var lose = 0;							// Variable victoire et défaite
+var chron;												//Contiendra le chronometre
 var chrono = document.getElementById("chrono");
 var temps = 1; var secondes = 60;
-var partieEnCours = true;
-var ready = true;
+var partieEnCours = true;								//Gere le lancement du chronometre
+var ready = true;										//Gere l'accès au clicks
 
-function chronometre(){
-	if(temps >= 0){
-		secondes--;
+function chronometre(){								//FONCTION CHRONOMETRE
+	if(temps >= 0){									
+		secondes--;																//On retire 1 à la variable secondes
 		if(secondes >= 10)
-			document.getElementById("chrono").innerHTML = temps+":"+secondes;
+			document.getElementById("chrono").innerHTML = temps+":"+secondes;	
 		else if(secondes < 10){
 			document.getElementById("chrono").innerHTML = temps+":0"+secondes;
 			if(secondes == 0){
 				secondes = 60;
-				temps--;
+				temps--;														//On retire 1 à la variable temps
 			}
 		}
 	}
 	else{
-			lose = 1;
+			lose = 1;															//Si temps == 0 c'est perdu !
 			victoire();
 			clearInterval(chron);
 		}
 }
-function recommencer(victoire){
+function recommencer(victoire){										//FONCTION RECOMMENCER
 	var retry = confirm("Vous avez "+victoire+" !\n Recommencer ?");
 	if(retry){
-		var ZJeu = document.getElementsByClassName("carte");
+		var ZJeu = document.getElementsByClassName("carte");				//Recupère toute les cartes de la Zone de jeu
 		for(i = 0; i < ZJeu.length; i++){
-			ZJeu[i].src = "img/dosCarte.jpg";
-			ZJeu[i].setAttribute("onclick", "retourne("+(i+1)+", '"+(i+1)+"')");
+			ZJeu[i].src = "img/dosCarte.jpg";								//modification de la source de chaque cartes
+			ZJeu[i].setAttribute("onclick", "retourne("+(i+1)+", '"+(i+1)+"')"); //réattribution de la fonction onclick
 		}
-		great = 0; lose = 0;
+		great = 0; lose = 0;												//réinitialisation de toutes le variables
 		ready = true;
 		partieEnCours = true;
 		temps = 1; secondes = 60;
@@ -50,8 +50,8 @@ function recommencer(victoire){
 
 function victoire(){
 	if(great == 6){
-		clearInterval(chron);
-		recommencer("gagné");
+		clearInterval(chron);									//Stop le chronometre <setInterval>
+		recommencer("gagné");									//Appel de la fonction recommencer
 		
 	}
 	else if(lose == 1){
@@ -61,10 +61,10 @@ function victoire(){
 	}
 }
 
-function shuffle(a) {
+function shuffle(a) {										//Fonction shuffle: melange le tableau tab
     var j, x, i;
     for (i = a.length; i; i--) {
-        j = Math.floor(Math.random() * i);
+        j = Math.floor(Math.random() * i);					
         x = a[i - 1];
         a[i - 1] = a[j];
         a[j] = x;
@@ -72,150 +72,42 @@ function shuffle(a) {
 }
 shuffle(tab);
 
-function retourne(id, name){
-	if(partieEnCours){
-		chron = setInterval("chronometre()", 1000);
-		partieEnCours = false
+function retourne(id, name){						//FONCTION RETOURNE
+	if(partieEnCours){									//Au premier click, lancement du chrono
+		chron = setInterval("chronometre()", 1000);		//Appelle la fonction chronometre toute les secondes
+		partieEnCours = false							//Rend la condition fausse pour éviter les multiple lancement de setInterval
 	}
-	if(ready){
-		name = document.getElementById(name);
-	 	switch(id){
-			case 1:
-				name.src =tab[0];
-				if(click == 0){
-					precedImg = name;
-					click = 1;
-				}
-				else{
-					click = 2;
-				}
-				break; 		
-			case 2:
-				name.src =tab[1];
-				if(click == 0){
-					precedImg = name;
-					click = 1;
-				}
-				else{
-					click = 2;
-				}
-				break;
-			case 3:
-				name.src =tab[2];
-				if(click == 0){
-					precedImg = name;
-					click = 1;
-				}
-				else{
-					click = 2;
-				}
-				break;
-			case 4:
-				name.src =tab[3];
-				if(click == 0){
-					precedImg = name;
-					click = 1;
-				}
-				else{
-					click = 2;
-				}
-				break;
-			case 5:
-				name.src =tab[4];
-				if(click == 0){
-					precedImg = name;
-					click = 1;
-				}
-				else{
-					click = 2;
-				}
-				break;
-			case 6:
-				name.src =tab[5];
-				if(click == 0){
-					precedImg = name;
-					click = 1;
-				}
-				else{
-					click = 2;
-				}
-				break;
-			case 7:
-				name.src =tab[6];
-				if(click == 0){
-					precedImg = name;
-					click = 1;
-				}
-				else{
-					click = 2;
-				}
-				break;
-			case 8:
-				name.src =tab[7];
-				if(click == 0){
-					precedImg = name;
-					click = 1;
-				}
-				else{
-					click = 2;
-				}
-				break;
-			case 9:
-				name.src =tab[8];
-				if(click == 0){
-					precedImg = name;
-					click = 1;
-				}
-				else{
-					click = 2;
-				}
-				break;
-			case 10:
-				name.src =tab[9];
-				if(click == 0){
-					precedImg = name;
-					click = 1;
-				}
-				else{
-					click = 2;
-				}
-				break;
-			case 11:
-				name.src =tab[10];
-				if(click == 0){
-					precedImg = name;
-					click = 1;
-				}
-				else{
-					click = 2;
-				}
-				break;
-			case 12:
-				name.src =tab[11];
-				if(click == 0){
-					precedImg = name;
-					click = 1;
-				}
-				else{
-					click = 2;
-				}
-				break; 			
-	 	}		
+	if(ready){											//Si ready = true, alors on peu cliquer
+		name = document.getElementById(name);			//Recupération de l'image qui appelle la fonction
+		for(i = 1; i <= tab.length; i++){
+			switch(id){									
+				case i:
+					name.src =tab[i-1];					//Attribution des sources du tableau à chaque images cliquées
+					if(click == 0){						//Au premier et seulement à celui-ci
+						precedImg = name;				//Attribution d'une valeur "sauvegarde" pour comparaison
+						click = 1;						//Empeche de réécrire la sauvegarde au second click
+					}
+					else{
+						click = 2;
+					}
+				break; 	
+			}
+		}	
 	 	if(name.src != precedImg.src && click == 2){
-	 		ready = false;
-			setTimeout(function(){
-				name.src = "img/dosCarte.jpg";
+	 		ready = false;								//On empeche les click durant la phase de comparaison
+			setTimeout(function(){						//Permet de lancer la fonction avec un retard choisit(ici 2secondes)
+				name.src = "img/dosCarte.jpg";			//Retourne les cartes pour les mettre à l'envers
 				precedImg.src = "img/dosCarte.jpg";
-				precedImg = "";
-				click = 0;
-				ready = true;
+				precedImg = "";							//Effacement de la "sauvegarde" pour la prochaine phase de click
+				click = 0;								
+				ready = true;							//Réautorisation des clicks
 			}, 2000);	
 		}
-		else if(name.src == precedImg.src && click == 2){
-			name.removeAttribute("onclick");
+		else if(name.src == precedImg.src && click == 2){		//Si 2 images sont identiques
+			name.removeAttribute("onclick");					//Suppression de l'attribut onclick sur les deux images
 			precedImg.removeAttribute("onclick");
 			click = 0;
-			great++;
+			great++;											//Ajoute un point
 		}
 	}
 	victoire();
